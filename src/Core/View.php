441783b0024091e10f7079c51ad6613e36b8c820
andresn-su <andre.s.04.get.up@gmail.com>
2022-09-23 13:44:25 -0300

    created View class and setted Twig

---
+++ src/Core/View.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Src\Core;
+
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+class View
+{
+    private FilesystemLoader $loader;
+    private Environment $twig;
+
+    /**
+     * Constructor
+     *
+     * @param string $path
+     */
+    public function __construct(string $path)
+    {
+        $this->loader = new FilesystemLoader($path);
+    }
+
+    /**
+     * Renders a view
+     *
+     * @param string $view
+     * @param array $data
+     * @return void
+     */
+    public function render(string $view, $data = [])
+    {
+        $this->twig = new Environment($this->loader);
+        return $this->twig->render($view, $data);
+    }
+}
